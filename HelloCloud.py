025aa8a93c21e227e0a21c398c110ecdf5331bfb
Patcharapong10 <patcharapong20newcom>
2021-01-26 16:46:59 +0700

    commit

--- HelloCloud.py
+++ HelloCloud.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return"Hello Patcharapong Happy New Year 2021"
+    return"Hello Patcharapong Happy New Year 2050"
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0',port=80)
