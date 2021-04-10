03974cc79e6f4b1ab5523bf46d57ffe1f549d75d
Patcharapong10 <patcharapong20new@gmail.com>
2021-04-10 21:06:54 +0700

    c

---
+++ testpy.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request,make_response,jsonify
+import pymongo
+
+class mongo_connection:
+  conn = None
+
+  def connect(self):
+    myclient = pymongo.MongoClient("mongodb://admin:GFHsax21310@10.100.2.83:27017")
+    mydb = myclient["flask_tutorial"]
+    self.conn = mydb["Ass1"]
+
+  def query(self, sql):
+      cursor = self.conn.find(sql)  
+      return cursor 
+
+db = mongo_connection()
+db.connect()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/Car', methods=['GET'])
+def getrestaurants(): 
+    carname = request.args.get('_name')
+    model = request.args.get('_model')
+    price = request.args.get('_price')
+
+    print(carname,model,price)
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0',port=9763,debug=True)
